e9f3ff7b12b823b7e896be540107bef13ad61891
Luis del Cid <luisdelcid@gmail.com>
2024-02-19 19:43:43 -0600

    LDC

--- src/php/functions.php
+++ src/php/functions.php
@@ -4,12 +4,12 @@
  * @return void
  */
 function ldc_wp_coundinary_add($size_name = '', $options = [], $use_cdn = false){
-
+    __singleton('LDC_WP_Cloudinary\Loader')->add($size_name, $options, $use_cdn);
 }
 
 /**
  * @return void
  */
 function ldc_wp_coundinary_config($config = ''){
-
+    __singleton('LDC_WP_Cloudinary\Loader')->add($config);
 }

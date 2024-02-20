26421110504e29914b7b1ac24d39751cad7a2a69
Luis del Cid <luisdelcid@gmail.com>
2024-02-19 19:39:17 -0600

    LDC

--- src/php/loader.php
+++ src/php/loader.php
@@ -1,1 +1,38 @@
-<?php
+<?php namespace LDC_WP_Cloudinary;
+
+final class Loader {
+
+	/**
+	 * @return array
+	 */
+	public function fl_builder_photo_sizes_select($sizes){
+
+		return $sizes;
+	}
+
+	/**
+	 * @return array|bool
+	 */
+	public function image_downsize($downsize, $id, $size){
+
+		return $downsize;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function image_size_names_choose($size_names){
+
+		return $size_names;
+	}
+
+    /**
+	 * @return void
+	 */
+	public function load(){
+        add_filter('fl_builder_photo_sizes_select', [$this, 'fl_builder_photo_sizes_select']);
+    	add_filter('image_downsize', [$this, 'image_downsize'], 10, 3);
+    	add_filter('image_size_names_choose', [$this, 'image_size_names_choose']);
+    }
+
+}
---
+++ src/php/src/php/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @return void
+ */
+function ldc_wp_coundinary_add($size_name = '', $options = [], $cdn = false){
+
+}
+
+/**
+ * @return void
+ */
+function ldc_wp_coundinary_config($config = ''){
+
+}
